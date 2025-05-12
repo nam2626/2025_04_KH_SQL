5e33a58617c798f266d2980b9fc212b6e983bf3b
nam2626 <nam2626@naver.com>
2025-05-12 12:12:25 +0900

    DML

---
+++ 2020_05_12.sql
@@ -0,0 +1,103 @@
+DROP TABLE PERSON;
+
+CREATE TABLE PERSON(
+    PNAME VARCHAR2(50),
+    PAGE NUMBER(3)
+);
+
+-- DML(Data Mainpulation Language) : 데이터 조작어
+--      데이터를 조회, 삭제, 수정, 추가
+--      SELECT, DELETE, UPDATE, INSERT
+SELECT SYSDATE FROM DUAL;
+
+--INSERT : 추가
+--	INSERT INTO 테이블명(컬럼1, 컬럼2, 컬럼3 ...)	
+--	VALUES(데이터1, 데이터2, 데이터3....)
+--	INSERT INTO 테이블명	
+--	VALUES(데이터1, 데이터2, 데이터3....)
+--	테이블 생성시 만든 모든 컬럼에 데이터를 저장
+--	데이터 순서는 CREATE 작성시 만든 컬럼의 순서대로 작성 
+
+-- PERSON 데이터 5건 추가 
+INSERT INTO PERSON(PNAME, PAGE) VALUES('홍길동', 20);
+INSERT INTO PERSON VALUES('이길동', 35);
+
+INSERT ALL 
+    INTO PERSON VALUES('김씨', 20)
+    INTO PERSON VALUES('이씨', 30)
+    INTO PERSON VALUES('박씨', 40)
+    INTO PERSON VALUES('정씨', 50)
+    INTO PERSON VALUES('곽씨', 60)
+SELECT * FROM DUAL;
+
+-- 데이터 조회 : SELECT
+-- SELECT 조회할 컬럼1, 조회할 컬럼2, ...
+-- FROM 조회할 테이블1, 조회할 테이블2, ...
+-- WHERE 조건절
+-- GROUP BY 그룹으로 묶을 컬럼1, 그룹으로 묶을 컬럼 2, ... [HAVING 조건절]
+-- ORDER BY 정렬할 기준 컬럼1 [ASC | DESC], 정렬할 기준 컬럼2 [ASC | DESC], ... 
+
+-- 전체 학생 데이터 조회
+SELECT * FROM STUDENT;
+-- 원하는 컬럼만 조회 - 이름, 학과명
+SELECT SNAME, MNAME FROM STUDENT;
+-- AS : 해당 컬럼에 별칭을 지정함, 함수나, 수식을 감추기 위해서
+SELECT MNAME, LENGTH(MNAME) AS MNAME_LENGTH FROM STUDENT;
+-- DISTINCT : 중복된 내용 제거해서 조회
+-- 학과명만 조회
+SELECT DISTINCT MNAME FROM STUDENT;
+-- 조건절
+--  관계연산자 : > < >= <= = <> !=
+--  논리연산자 : AND OR NOT
+-- STUDENT 테이블에서 평점이 4.0 이상인 학생만 조회
+SELECT * FROM STUDENT WHERE SCORE >= 4.0;
+-- STUDENT 테이블에서 학과명이 심리학과가 아닌 학생들만 조회
+SELECT * FROM STUDENT WHERE NOT MNAME LIKE '심리학과';
+SELECT * FROM STUDENT WHERE MNAME <> '심리학과';
+-- STUDENT 테이블에서 학과명이 전자공학과, 기계공학과인 학생들만 조회
+SELECT * FROM STUDENT WHERE MNAME LIKE '전자공학과' OR MNAME LIKE '기계공학과';
+-- 컬럼 IN(값1, 값2, ...) 해당 컬럼 값이 IN안에 존재하면 TRUE, 없으면 FALSE
+-- 비교하는 연산자는 = 으로 비교
+SELECT * FROM STUDENT WHERE MNAME IN('전자공학과', '기계공학과');
+-- STUDENT 테이블에서 학과명이 전자공학과, 기계공학과인 학생들을 제외하고 조회
+SELECT * FROM STUDENT WHERE MNAME NOT IN('전자공학과', '기계공학과');
+-- 와일드 카드 문자
+-- % : 글자개수 0개 이상 올 수 있다.
+-- 학생이름이 박씨인 학생을 조회
+SELECT * FROM STUDENT WHERE SNAME LIKE '박%';
+-- 학과명에 공학이 들어가는 학생들만 조회
+SELECT * FROM STUDENT WHERE MNAME LIKE '%공학%';
+-- 학생이름이 빈으로 끝나는 학생들만 조회
+SELECT * FROM STUDENT WHERE SNAME LIKE '%빈';
+-- _ : 글자 개수 1개인 경우
+SELECT * FROM STUDENT WHERE MNAME LIKE '__공학';
+-- || 양쪽의 데이터를 하나의 문자열로 합쳐주는 연산자
+SELECT PNAME || PAGE FROM PERSON;
+SELECT PNAME || ',' || PAGE FROM PERSON;
+
+SELECT * FROM USER_TABLES;
+-- 전체 테이블 삭제하기 위한 DROP TABLE 테이블명; <-- 으로 조회
+-- DROP TABLE STUDENT;
+-- DROP TABLE PERSON;
+-- ...
+SELECT 'DROP TABLE ' || TABLE_NAME || ';' FROM USER_TABLES;
+-- BETWEEN a AND b : a부터 b까지
+SELECT * FROM STUDENT WHERE SCORE BETWEEN 3.0 AND 3.25;
+
+-- 데이터 삭제 : DELETE 
+--      DELETE FROM 테이블명 WHERE 조건절;
+DELETE FROM PERSON;
+ROLLBACK;
+SELECT * FROM PERSON;
+
+DELETE FROM PERSON WHERE PAGE >= 40;
+-- 학생 데이터 중 평점이 0.5 이하인 학생 삭제
+DELETE FROM STUDENT WHERE SCORE <= 0.5;
+SELECT * FROM STUDENT WHERE SCORE <= 0.5;
+
+
+
+
+
+
+
