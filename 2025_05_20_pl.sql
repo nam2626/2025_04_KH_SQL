ed917386ed1a8bb245b374af9e4a12448e23b5d4
nam2626 <nam2626@naver.com>
2025-05-20 16:48:22 +0900

    프로시저

--- 2025_05_20_pl.sql
+++ 2025_05_20_pl.sql
@@ -217,6 +217,85 @@ INSERT INTO MAJOR VALUES('S01','1234');
 DELETE FROM MAJOR WHERE MNO = 'S01';
 UPDATE MAJOR SET MNAME = '디지털콘텐츠학과' WHERE MNO = 'M02';
 SELECT * FROM DATA_LOG;
+---------------------------------------------------------------------
+-- 프로시저
+--  SQL 쿼리문으로 로직을 조합해서 사용하는 데이터베이스 코드
+--  SQL문과 제어문을 이용해서, 데이터를 검색, 삽입, 수정, 삭제를 할 수 있음.
+--  결과를 외부로 전달할 수도 있음.
+--  하나의 트랜잭션 구성시 사용함.
+---------------------------------------------------------------------
+-- 매개변수가 없는 프로시저
+CREATE OR REPLACE PROCEDURE PROCEDURE_EX1
+IS
+    -- 변수 선언
+    TEST_VAR VARCHAR2(100);
+BEGIN
+    -- 실행부
+    TEST_VAR := 'Hello World';
+    DBMS_OUTPUT.PUT_LINE(TEST_VAR);
+END;
+/
+DECLARE
+    -- 사용할 변수
+BEGIN
+    -- 실행 영역
+    PROCEDURE_EX1();
+END;
+/
+-- 매개변수가 있는 프로시저
+CREATE OR REPLACE PROCEDURE PROCEDURE_EX2(
+    V_PID IN VARCHAR2,
+    V_PNAME IN VARCHAR2,
+    V_PAGE IN NUMBER)
+IS
+    TEST_VAR VARCHAR2(100);
+BEGIN
+    -- PERSON 테이블에 프로시저로 받아온 데이터 INSERT 문 실행
+    DBMS_OUTPUT.PUT_LINE(V_PID || ' ' || V_PNAME || ' ' || V_PAGE );
+    INSERT INTO PERSON VALUES(V_PID,V_PNAME,V_PAGE);
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('ERROR 발생');
+        ROLLBACK;
+END;
+/
+BEGIN
+    PROCEDURE_EX2('00011','김아두',33);
+END;
+/
+SELECT * FROM PERSON;
+/
+--값을 외부로 전달하는 프로시저
+CREATE OR REPLACE PROCEDURE PROCEDURE_EX3(
+	NUM IN NUMBER,
+	RESULT OUT NUMBER)
+IS
+	I NUMBER;
+	USER_EXCEPTION EXCEPTION;
+BEGIN
+	IF NUM <= 0 THEN
+		RAISE USER_EXCEPTION;
+	END IF;
+	--반복문 이용해서 1~NUM까지 곱하는 팩토리얼 계산
+	--결과 값을 RESULT에 저장
+	RESULT := 1;
+	FOR I IN 1 .. NUM
+	LOOP
+		RESULT := RESULT * I;
+	END LOOP;
+EXCEPTION
+	WHEN OTHERS THEN
+		DBMS_OUTPUT.PUT_LINE('숫자는 0보다 커야합니다.');
+		RESULT := -1;
+END;
+
+
+
+
+
+
+
 
 
 
